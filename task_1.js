1ba42928daee8b6aa747df3c208bda4808cb4722
Anna <62948315+aattentionn@users.noreply.github.com>
2025-01-28 20:00:33 +0300

    rewrote task_1.js
    добавила проверки и сделала решение только с числами (переписала с помощью gpt)

--- task_1.js
+++ task_1.js
@@ -1,4 +1,4 @@
-//1. Посчитать сумму цифр трехзначного числа. Трехзначные числа от 100 до 999. Чтобы посчитать сумму чисел, нам нужно разделить число на цифры и сложить их.
+//1. Посчитать сумму цифр трехзначного числа. Трехзначные числа от 100 до 999.
 
 
 function sumNums(num) {
@@ -7,10 +7,22 @@ function sumNums(num) {
         return "Введи трехзначное число!"
     };
 
-       let symbols = String(num).split('');
-       let sum = symbols.reduce((acc, symbol) => acc + parseInt(symbol), 0);
-       return sum;
+    if (typeof num !== "number" || isNaN(num)) {
+        return "Это строка!"
+    };
+
+    if (num % 1 !== 0) {
+        return "Введи целое число!"
+    };
+
+    let hundreds = Math.floor(num / 100);
+    let tens = Math.floor((num % 100) / 10);
+    let units = num % 10;
     
+    return hundreds + tens + units;   
 }
 
 sumNums(100);
+sumNums(256);
+sumNums(123.3);
+sumNums("adfdf");
