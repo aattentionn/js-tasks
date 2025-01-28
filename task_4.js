a5a2c7774eaa52df59740d43b006c034717a4282
Anna <62948315+aattentionn@users.noreply.github.com>
2025-01-28 20:25:25 +0300

    rewrote task_4.js
    заменила num * num на num * 2

--- task_4.js
+++ task_4.js
@@ -4,7 +4,7 @@
 const arr = [1, 15, 21, 34, 46, 58, 79, 83, 124];
 
 const newArr = arr.map(function (num){
-    return num * num; // или num возведенная в степень 2
+    return num * 2;
 })
 
 console.log(newArr);
